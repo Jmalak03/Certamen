a6c4e03f7fd996055814da184c6429acebcc4616
Jmalak03 <jossidey@hotmail.es>
2023-10-31 21:39:23 -0600

    Add project files.

---
+++ Controllers/VotacionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Certamen.Controllers
+{
+    public class VotacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        private DbContext _context;
+
+        public VotacionController(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Votar(int Id)
+        {
+            var concursante = _context.Concursante.FirstOrDefault(c => c.Id == Id);
+            if (concursante != null)
+            {
+                concursante.Votos++;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+
+
+}
+
+
---
+++ Models/VotacionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Certamen.Models
+{
+    public class Voto
+    {
+        public int VotoId { get; set; }
+        public string ConcursanteId { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class Concursante
+    {
+        public int ConcursanteId { get; set; }
+        public string Concursantes { get; set; }
+        public int Votos { get; set; }
+    }
+
+}
+
